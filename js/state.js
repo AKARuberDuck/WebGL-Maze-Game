c5c85ae08be59500608b317309b9df55bb22743e
AKARuberDuck <ss2875356@gmail.com>
2025-06-27 20:40:44 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,15 @@
+export const GameState = {
+  MENU: 'menu',
+  PLAYING: 'playing',
+  GAMEOVER: 'gameover'
+};
 
+export let currentState = GameState.MENU;
+
+export function setState(newState) {
+  currentState = newState;
+
+  document.getElementById('startScreen').classList.toggle('hidden', newState !== GameState.MENU);
+  document.getElementById('hud').classList.toggle('hidden', newState !== GameState.PLAYING);
+  document.getElementById('gameOverScreen').classList.toggle('hidden', newState !== GameState.GAMEOVER);
+}
